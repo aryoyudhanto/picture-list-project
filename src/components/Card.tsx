37097422a4f8a4fb6fd4b7929878ee6733504e38
aryoyudhanto <aryoyudhanto26399@gmail.com>
2023-03-13 19:43:28 +0700

    feat: add reusable card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+
+interface CardProps {
+  id?: number;
+  name?: string;
+  username?: string;
+  onclickDetail?: () => void;
+  onclickFav?: () => void;
+}
+
+const Card: FC<CardProps> = ({
+  id,
+  name,
+  username,
+  onclickDetail,
+  onclickFav,
+}) => {
+  return (
+    <div className="card bg-base-100 shadow-xl image-full mt-3 md:mt-0">
+      <figure>
+        <img
+          src="https://www.adobe.com/content/dam/cc/us/en/creative-cloud/photography/discover/landscape-photography/CODERED_B1_landscape_P2d_714x348.jpg.img.jpg"
+          onClick={onclickDetail}
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title" onClick={onclickDetail}>
+          {name}
+        </h2>
+        <p className="lowercase" onClick={onclickDetail}>
+          @{username}
+        </p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary" onClick={onclickFav}>
+            Buy Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
